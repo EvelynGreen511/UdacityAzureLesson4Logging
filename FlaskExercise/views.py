e8ec65a97889f5fb7f23b7075583ed0eedb71e24
Evelyn Kolczynski <evelyn.kolczynski@outlook.com>
2021-01-16 18:41:12 +0100

    Solution to Excercise

--- FlaskExercise/views.py
+++ FlaskExercise/views.py
@@ -7,6 +7,15 @@ def home():
     log = request.values.get('log_button')
     # TODO: Appropriately log the different button presses
     #   with the appropriate log level.
+    if log:
+        if log == 'info':
+            app.logger.info('No issues')
+        elif log == 'warning':
+            app.logger.warning('Warning occured')
+        elif log == 'error':
+            app.logger.error('Error occured')
+        elif log == 'critical':
+            app.logger.critical('Critical error occured')
     return render_template(
         'index.html',
         log=log
